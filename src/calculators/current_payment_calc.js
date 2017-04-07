571ac1e3bc77ab754b1ea5c1532e2dac02ccfa1e
thrav <travismarceau@gmail.com>
2017-04-06 22:47:17 -0700

    added example code to the bottom for digestion later
    shift to more simple approach for handling these fields

--- src/calculators/current_payment_calc.js
+++ src/calculators/current_payment_calc.js
@@ -77,3 +77,23 @@ CurrentPaymentCalc = connect(
 )(CurrentPaymentCalc);
 
 export default CurrentPaymentCalc;
+
+// handleEmailChange: function(e) {
+//    this.setState({email: e.target.value});
+// },
+// handlePasswordChange: function(e) {
+//    this.setState({password: e.target.value});
+// },
+// render : function() {
+//       return (
+//         <form>
+//           <input type="text" name="email" placeholder="Email" value={this.state.email} onChange={this.handleEmailChange} />
+//           <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handlePasswordChange}/>
+//           <button type="button" onClick={this.handleLogin}>Login</button>
+//         </form>);
+// },
+// handleLogin: function() {
+//     console.log("EMail: " + this.state.email);
+//     console.log("Password: " + this.state.password);
+// }
+// https://facebook.github.io/react/docs/forms.html
